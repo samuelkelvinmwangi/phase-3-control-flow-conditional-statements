a15baf996dd411e2a5e56f50c32a8fee7de53aac
samuel <samuelkelvinmwangi@gmail.com>
2023-06-21 14:06:07 +0300

    finishing the assingnment on operators

--- control_flow.rb
+++ control_flow.rb
@@ -1,16 +1,47 @@
 def admin_login(username, password)
-  # your code here
+    if username.downcase == 'admin' && password == '12345'
+    'Access granted'
+  else
+    'Access denied'
+  end
 end
 
 def hows_the_weather(temperature)
-  # your code here
+  if temperature < 40
+    "It's brisk out there!"
+  elsif temperature >= 40 && temperature <= 65
+    "It's a little chilly out there!"
+  elsif temperature > 85
+    "It's too dang hot out there!"
+  else
+    "It's perfect out there!"
+  end
 end
 
-def fizzbuzz(num)
-  # your code here
+def fizzbuzz(number)
+  if number % 3 == 0 && number % 5 == 0
+    'FizzBuzz'
+  elsif number % 3 == 0
+    'Fizz'
+  elsif number % 5 == 0
+    'Buzz'
+  else
+    number
+  end
 end
 
-def calculator(operation, num1, num2)
-  # your code here
+def calculator(operator, num1, num2)
+  case operator
+  when '+'
+    num1 + num2
+  when '-'
+    num1 - num2
+  when '*'
+    num1 * num2
+  when '/'
+    num1 / num2
+  else
+    puts 'Invalid operation!'
+    nil
+  end
 end
-
